09a7777e92b3b6ed2c25b1646255b4296630bbea
ShortyDev <shortydev8274@gmail.com>
2023-03-03 13:06:20 +0100

    02_js - 02_Strings_und_Zahlen

---
+++ 02_js/02_Strings_und_Zahlen/script.js
@@ -0,0 +1,28 @@
+let a = 7
+let b = 8
+
+let ab = a + b
+
+let c = 7.4
+let d = 7.5
+
+let result = c + d
+console.log(result)
+
+let name = "Max"
+console.log(name)
+console.log("Das Ergebnis von " + a + " + " + b + " ist " + ab)
+
+// AUFGABE
+// 1)
+a = 17.4
+b = 3.9
+result = 17.4 / 3.9
+
+console.log("The result of " + a + " / " + b + " is " + result)
+
+// 2)
+let vorname = "Max"
+let nachname = "Mustermann"
+
+console.log(`Ich heiße ${vorname} ${nachname}`)
